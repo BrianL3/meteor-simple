2bdcf9ee7c8a96aab01511a7667058539eb0f99e
Brian Ledbetter <Brian@Wealthfile.com>
2016-08-11 14:35:31 -0700

    Added user log in

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import '../imports/api/tasks.js';
+import '../imports/startup/accounts-config.js';
 import todosList from '../imports/components/todosList/todosList';
- 
+
+
 angular.module('simple-todos', [
     angularMeteor,
-  todosList.name
+  todosList.name,
+  'accounts.ui'
 ]);
